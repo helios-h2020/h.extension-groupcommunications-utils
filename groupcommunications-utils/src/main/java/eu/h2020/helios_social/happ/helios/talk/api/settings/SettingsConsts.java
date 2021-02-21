58d6a6b7acc901aa5d2e761d03a0ab340b12f73c
vasgat <vasgat@iti.gr>
2021-02-21 19:18:14 +0200

    adding SettingsConsts

---
+++ groupcommunications-utils/src/main/java/eu/h2020/helios_social/happ/helios/talk/api/settings/SettingsConsts.java
@@ -0,0 +1,7 @@
+package eu.h2020.helios_social.happ.helios.talk.api.settings;
+
+public interface SettingsConsts {
+
+    public static final String SETTINGS_NAMESPACE = "android-ui";
+    public static final String PREF_CONTENT_PROFILING = "pref_profiling_method";
+}
